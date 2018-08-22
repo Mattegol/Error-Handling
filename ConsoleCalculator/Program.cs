39d9b8de5dda12c7c4d5d09e7b5a39fc61cb7f1a
Mats Egnell <mattegol.egnell@gmail.com>
2018-08-22 10:52:40 +0200

    Add Calculator

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ConsoleCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+            var calculator = new Calculator();
+            int result = calculator.Calculate(number1, number2, operation);
+            DisplayResult(result);
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result)
+        {
+            WriteLine($"Result is: {result}");
         }
     }
 }
