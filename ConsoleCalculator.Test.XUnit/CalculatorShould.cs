e6aec63431ef345822151758dd1f722150352da7
Mats Egnell <mattegol.egnell@gmail.com>
2018-08-23 10:11:43 +0200

    Add testing with XUnit

---
+++ ConsoleCalculator.Test.XUnit/CalculatorShould.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace ConsoleCalculator.Test.XUnit
+{
+    public class CalculatorShould
+    {
+
+        [Fact]
+        public void ThrowWhenUnsupportedOperation()
+        {
+            var sut = new Calculator();
+
+            Assert.Throws<CalculationOperationNotSupportedException>(
+                () => sut.Calculate(1, 1, "+"));
+
+            Assert.ThrowsAny<CalculationException>(
+                () => sut.Calculate(1, 1, "+"));
+
+            var ex = Assert.Throws<CalculationOperationNotSupportedException>(
+                () => sut.Calculate(1, 1, "+"));
+
+            Assert.Equal("+", ex.Operation);
+        }
+        
+    }
+}
